1c5f2a6202bb77146bd1eaf2033c6578038d4bee
felixx78 <petrenkov605@gmail.com>
2025-03-02 03:29:18 +0200

    update useInputStore

--- src/store/useInputStore.tsx
+++ src/store/useInputStore.tsx
@@ -1,15 +1,19 @@
 import { create } from "zustand";
+import Option from "../types/Option";
 
 type InputStore = {
-  values: string[];
-  setValues: (v: string[]) => void;
+  values: (string | Option)[];
+  setValues: (v: (string | Option)[]) => void;
 };
 
+const storedValues = localStorage.getItem("values");
+
 const useInputStore = create<InputStore>((set) => ({
-  values: (localStorage.getItem("values") || []) as string[],
-  setValues: (v: string[]) => {
-    set({ values: v });
-    localStorage.setItem("values", JSON.stringify(v));
+  values: (storedValues ? JSON.parse(storedValues) : [""]) as InputStore["values"],
+  setValues: (v: InputStore["values"]) => {
+    const formatedValues = [...v.filter(Boolean), ""];
+    set({ values: formatedValues });
+    localStorage.setItem("values", JSON.stringify(formatedValues));
   },
 }));
 
