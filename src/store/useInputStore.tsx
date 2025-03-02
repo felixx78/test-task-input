7b8706e958021fdabf4a2d619ff3489ee7604810
felixx78 <petrenkov605@gmail.com>
2025-03-02 03:46:03 +0200

    fix double empty string in the end useInputStore

--- src/store/useInputStore.tsx
+++ src/store/useInputStore.tsx
@@ -8,10 +8,16 @@ type InputStore = {
 
 const storedValues = localStorage.getItem("values");
 
+const formatValues = (values: InputStore["values"]) => {
+  const filtered = values.filter(Boolean);
+  if (typeof filtered[filtered.length - 1] === "object") filtered.push("");
+  return filtered;
+};
+
 const useInputStore = create<InputStore>((set) => ({
   values: (storedValues ? JSON.parse(storedValues) : [""]) as InputStore["values"],
   setValues: (v: InputStore["values"]) => {
-    const formatedValues = [...v.filter(Boolean), ""];
+    const formatedValues = formatValues(v);
     set({ values: formatedValues });
     localStorage.setItem("values", JSON.stringify(formatedValues));
   },
